104216ad495cc50cc970b9a45b0b78b2f025f4ab
Prasad Patil <prasadpatil131@gmail.com>
2019-05-18 05:05:24 +0530

    Update main_.py

--- main_.py
+++ main_.py
@@ -3,10 +3,10 @@ import tweepy
 from textblob import TextBlob
 
 # Keys and Tokens
-consumer_key = 'WgjRmUXX8BoThbDrDnfQbOEGV'
-consumer_secret = 'wPGxE2J5KL5u0D3geZFawIAk2TuZSG37ov0DRk04E6xpwZqSuH'
-access_token ='1121720100959084544-SPNB1CFfypRfi329u4hitaAPVx7raq'
-access_token_secret = 'lVrZMC7QZ2FQCSaYwpgvFJDwJcX09jLeFq9dknccLL7vF'
+consumer_key = 'XxxxxxxxxxxxxxxxxxxxxxxxX'
+consumer_secret = 'XxxxxxxxxxxxxxxxxxxxX'
+access_token ='XxxxxxxxxxxxxxxxxxxxxxxxX'
+access_token_secret = 'XxxxxxxxxxxxxxxxxxxX'
 
 # Authenticating Handler
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
